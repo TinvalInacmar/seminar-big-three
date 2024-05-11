710a54177e725353935f319ca998d36356907d21
TinvalInacmar <vm52396@fer.hr>
2024-05-11 19:12:52 +0200

    a

--- APViT-main/mmcls/datasets/ferplus.py
+++ APViT-main/mmcls/datasets/ferplus.py
@@ -5,10 +5,10 @@ from .base_fer_dataset import BaseFerDataset
 @DATASETS.register_module()
 class FERPlus(BaseFerDataset):
     DATASET_CLASSES = ['Neutral', 'Happiness', 'Surprise', 'Sadness', 'Anger', 'Disgust', 'Fear', 'Contempt']
-    CONVERT_TABLE = (0,1,2,2,2,2,2,2) #(6, 4, 5, 3, 0, 1, 2, 7)
+    CONVERT_TABLE = (6, 4, 5, 3, 0, 1, 2, 7) #(0,1,2,2,2,2,2,2)
     
     def convert_gt_label(self, i:int):
         """# dataset -> FER_BASE_CLASSES"""
         convert_table = self.CONVERT_TABLE
-        #assert sum(convert_table) == sum([i for i in range(3)])
+        assert sum(convert_table) == sum([i for i in range(7)])
         return convert_table[i]
