c7deb4b4b14838d809bd40d7c68b32cae0b2ab8f
TinvalInacmar <vm52396@fer.hr>
2024-05-12 19:12:39 +0200

    a

--- APViT-main/mmcls/datasets/ferplus.py
+++ APViT-main/mmcls/datasets/ferplus.py
@@ -5,6 +5,6 @@ from .base_fer_dataset import BaseFerDataset
 @DATASETS.register_module()
 class FERPlus(BaseFerDataset):
     DATASET_CLASSES = ["Neutral", "Happiness", "Other"]
-    CONVERT_TABLE = (2, 2, 2, 2, 1, 2, 0, 2) 
+    CONVERT_TABLE = (0, 1, 2, 2, 1, 2, 0, 2) 
 #FER_BASE_CLASSES = ['Anger', 'Disgust', 'Fear', 'Sadness', 'Happiness', 'Surprise', 'Neutral', 'Contempt']
 
